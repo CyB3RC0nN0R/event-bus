88ba515cbf4dda083d52648f6cfdbf3bd3376098
kske <kai@kske.dev>
2020-09-02 16:15:31 +0200

    Add all relevant classes and event bus logic

---
+++ src/main/java/dev/kske/eventbus/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains the public API and implementation of the event bus library.
+ *
+ * @author Kai S. K. Engelbart
+ * @since 0.0.1
+ * @see dev.kske.eventbus.Event
+ * @see dev.kske.eventbus.EventBus
+ */
+package dev.kske.eventbus;
