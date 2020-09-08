5f88ad6095c501d8fdf02cd0ba208c4a2149bad3
kske <kai@kske.dev>
2020-09-08 17:06:45 +0200

    Add a module descriptor for the entire library

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Contains the public API and implementation of the event bus library.
+ *
+ * @author Kai S. K. Engelbart
+ * @since 0.0.3
+ * @see dev.kske.eventbus.Event
+ * @see dev.kske.eventbus.EventBus
+ */
+module dev.kske.eventbus {
+
+	exports dev.kske.eventbus;
+}
