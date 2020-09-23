603a83864028b92eb3cb81286d9209e245c3e781
delvh <leon@kske.dev>
2020-09-23 20:55:16 +0200

    Add Missing Javadoc to EventBusException (#1)
    Removed annoying Javadoc error
    Reviewed-on: https://git.kske.dev/zdm/event-bus/pulls/1
    Reviewed-by: kske <kai@kske.dev>

--- src/main/java/dev/kske/eventbus/EventBusException.java
+++ src/main/java/dev/kske/eventbus/EventBusException.java
@@ -1,8 +1,9 @@
 package dev.kske.eventbus;
 
 /**
- * This runtime exception is thrown when an event bus error occurs. This can either occur while
- * registering event listeners with invalid handlers, or when an event handler throws an exception.
+ * This runtime exception is thrown when an event bus error occurs. This can
+ * either occur while registering event listeners with invalid handlers, or when
+ * an event handler throws an exception.
  *
  * @author Kai S. K. Engelbart
  * @since 0.0.1
@@ -11,10 +12,21 @@ public class EventBusException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Creates a new event bus exception.
+	 *
+	 * @param message the message to display
+	 * @param cause   the cause of this exception
+	 */
 	public EventBusException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * Creates a new event bus exception.
+	 *
+	 * @param message the message to display
+	 */
 	public EventBusException(String message) {
 		super(message);
 	}
